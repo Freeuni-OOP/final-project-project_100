b08fc4d6215d492ecf6d37fb0c4c88d39929c9b1
potatoSalad21 <eeperpeepo@gmail.com>
2026-06-12 11:45:12 +0400

    feat: User repository interface implemented

---
+++ Quiz-Website/src/main/java/com/freeuni/proj_100/quizwebsite/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.freeuni.proj_100.quizwebsite.repository;
+
+import com.freeuni.proj_100.quizwebsite.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmail(String email);
+}
