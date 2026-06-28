f65a6ec1633fd0b39dbb50a06d71f6b1abb548d4
potatoSalad21 <eeperpeepo@gmail.com>
2026-06-28 22:53:27 +0400

    feat: site layout with a navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
+import styles from '../styles/navbar.module.css'
+
+export default function Navbar() {
+
+}
