2f918783556a037dbb980ed96bea7761c5b855ae
potatoSalad21 <eeperpeepo@gmail.com>
2026-07-10 16:37:19 +0400

    feat: getting questions + answers for rendering

--- Quiz-Website/src/main/java/com/freeuni/proj_100/quizwebsite/repository/QuizRepository.java
+++ Quiz-Website/src/main/java/com/freeuni/proj_100/quizwebsite/repository/QuizRepository.java
@@ -1,8 +1,13 @@
 package com.freeuni.proj_100.quizwebsite.repository;
 
+import com.freeuni.proj_100.quizwebsite.model.QuestionEntity;
 import com.freeuni.proj_100.quizwebsite.model.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Repository interface for Quiz entity.
@@ -16,4 +21,10 @@ public interface QuizRepository extends JpaRepository<Quiz, Integer> {
      * @return list of quizzes created by that user.
      */
     List<Quiz> findByCreatorId(Integer creatorId);
+
+    @Query("SELECT DISTINCT q FROM Quiz q LEFT JOIN FETCH q.questions WHERE q.id = :id")
+    Optional<Quiz> findByIdWithQuestions(@Param("id") Integer id);
+
+    @Query("SELECT DISTINCT q FROM QuestionEntity q LEFT JOIN FETCH q.answers WHERE q.quiz.id = :quizId")
+    List<QuestionEntity> findQuestionsWithAnswers(@Param("quizId") Integer quizId);
 }
